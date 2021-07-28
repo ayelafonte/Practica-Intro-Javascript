7a027b9593f27e867b27f74dc0901378981858da
Ayelen Lafonte <ayelenlafonte@gmail.com>
2021-07-28 19:22:45 +0200

    Creo index

---
+++ index.js
@@ -0,0 +1,44 @@
+
+// const teams = ['Bélgica', 'Portugal','Italia', 'Austria', 'Francia', 'Suiza', 'Croacia', 'España', 'Suecia', 'Ucrania', 'Inglaterra', 'Alemania', 'Paises Bajos', 'República Checa', 'Gales', 'Dinamarca'];
+
+
+
+// constructor(nombre, teams = [], config = {})
+// {
+//     this.nombre = nombre;
+//     this.setupTeams(teams);
+//     this.setup (config);
+// }
+
+// setup(config)
+// {
+//     const defaultConfig = { rounds: 1 };
+//     this.config = Object.assign(defaultConfig, config)
+// }
+ 
+//  setupTeams(teamsNames)
+//  {
+//     this.teams = [];
+//     for (const teamName of teamNames) {
+//         const team = this.customizeTeam(teamName);
+//         this.teams.push(team);
+//     }
+//     this.teams.shuffle();
+// }
+
+// customizeTeam (teamName)
+// {
+//     return {
+//         nombre: teamName,
+//         goals: 0
+//     }
+// }
+
+
+console.log ('==========================================================');
+console.log ('================== COMIENZA LA EUROCOPA ==================')
+console.log ('==========================================================')
+console.log ('\n')
+console.log ('===== OCTAVOS DE FINAL =====')
+
+
